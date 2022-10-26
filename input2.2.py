ce89eea1445af971503154f7790bc991a6f5e373
EdMraz <eduard.mraz05@gmail.com>
2022-10-26 18:01:28 +0200

    new file

--- input2.2.py
+++ input2.2.py
@@ -1,6 +1,15 @@
-def uloha2(ret):
+def uloha2(string:str):
     counter = 0
-    #for i in range(0,10):
+    i = 0
+    while i < len(string):
+        for x in string[i]:
+            if x.isnumeric() == True:
+                counter+=1
+                i+=1
+            else:
+                i+=1
+    return counter
 
-
-print(uloha2("abcd 15 20 35"))
+print(uloha2("Na farme mame od roku 2012 12 kráv a 230 oviec."))
+print(uloha2("Skutok sa stal."))
+print(uloha2("snehulienka a 7 trpazlikov."))
